77bb3b8da0405e2ddbb17be5ca2e3b950700a613
ivanxalie <ivanxalie@gmail.com>
2025-03-15 23:24:12 +0300

    Add repetition test with DI

--- src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -1,9 +1,7 @@
 package guru.springframework.sfgpetclinic.model;
 
 import guru.springframework.sfgpetclinic.ModelTests;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -26,5 +24,10 @@ class PersonTest implements ModelTests {
     @DisplayName("My Repeated Test")
     @RepeatedTest(value = 10, name = "{displayName}:{currentRepetition}/{totalRepetitions}")
     void myRepeatedTest() {
+    }
+
+    @RepeatedTest(5)
+    void myRepeatedTestWithDI(TestInfo testInfo, RepetitionInfo repetitionInfo) {
+        System.out.printf("%s : %s%n", testInfo.getDisplayName(), repetitionInfo.getCurrentRepetition());
     }
 }
