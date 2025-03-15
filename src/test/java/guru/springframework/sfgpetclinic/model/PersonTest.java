bd4a76007387eda5f2eda5ce8d2a57d410ad9f81
ivanxalie <ivanxalie@gmail.com>
2025-03-15 13:58:17 +0300

    Add PersonTest

---
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -0,0 +1,21 @@
+package guru.springframework.sfgpetclinic.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PersonTest {
+
+    @Test
+    void groupedAssertions() {
+        // give
+        Person person = new Person(1L, "Joe", "Black");
+
+        // then
+        assertAll("Test Props Set",
+                () -> assertEquals("Joe", person.getFirstName(), "First Name Failed!"),
+                () -> assertEquals("Black", person.getLastName(), "Last Name Failed!")
+        );
+    }
+}
