0e01e2cda7b9e01cfb0c9147032e0243f5fa4f05
ivanxalie <ivanxalie@gmail.com>
2025-03-15 18:42:47 +0300

    Modify IndexController and enableOn tests

--- src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -1,10 +1,12 @@
 package guru.springframework.sfgpetclinic.controllers;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.condition.*;
+
+import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 class IndexControllerTest {
     private IndexController indexController;
@@ -25,5 +27,66 @@ class IndexControllerTest {
     void oupsHandler() {
         assertThrows(ValueNotFoundException.class, () -> indexController.oopsHandler(),
                 "Should throw ValueNotFoundException");
+    }
+
+    @Test
+    @Disabled("Demo of timeout")
+    void testTimeout() {
+        assertTimeout(Duration.ofMillis(100), () -> Thread.sleep(2000));
+    }
+
+    @Test
+    @Disabled("Demo of timeout")
+    void testTimeoutPreempt() {
+        assertTimeoutPreemptively(Duration.ofMillis(100),
+                () -> Thread.sleep(2000));
+    }
+
+    @Test
+    void testAssumptionTrue() {
+        assumeTrue("GURU".equalsIgnoreCase(System.getProperty("GURU_RUNTIME")));
+        assertFalse(true);
+    }
+
+    @Test
+    void testAssumptionFalse() {
+        assumeTrue("GURU".equalsIgnoreCase("GURU"));
+        assertFalse(true);
+    }
+
+    @Test
+    @EnabledOnOs(OS.MAC)
+    void testMeOnMacOS() {
+
+    }
+
+    @Test
+    @EnabledOnOs(OS.WINDOWS)
+    void testMeOnWindows() {
+
+    }
+
+    @Test
+    @EnabledOnJre(JRE.JAVA_8)
+    void testMeOnJava8() {
+
+    }
+
+    @Test
+    @EnabledOnJre(JRE.JAVA_11)
+    void testMeOnJava11() {
+
+    }
+
+    @Test
+    @EnabledIfEnvironmentVariable(named = "USERNAME", matches = "ivan")
+    void testIfUserMe() {
+
+    }
+
+    @Test
+    @EnabledIfEnvironmentVariable(named = "USERNAME", matches = "john")
+    void testIFUserJohn() {
+
     }
 }
